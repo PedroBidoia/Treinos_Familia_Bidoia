0dd0b8f08d2439632d43db898ec97e9c9b1e05d8
Pedro Bidoia <pedro.bidoias@gmail.com>
2024-10-06 13:28:57 -0300

    FixBug001

--- exercicios.js
+++ exercicios.js
@@ -1,20 +1,20 @@
 const todosExercicios = {
     "peito": [
-        { id: "supino_reto_halteres", nome: "Supino Reto com Halteres", imagem: "imgs\supino_reto_halteres.png", series: "4x10", descricao: "Pegue o banco, deixe reto, e empurre halteres para cima." },
-        { id: "supino_inclinado_halteres", nome: "Supino Inclinado com Halteres", imagem: "imgs\supino_inclinado_halteres.png", series: "4x10", descricao: "Pegue o banco, deixe Inclinado, e empurre halteres para cima."}, 
-        { id: "peckdeck_flexao", nome: "PeckDeck com Flexão", imagem: "imgs\peckdeck_flexao.png", series: "3x10+10", descricao: "Faça o PeckDeck e logo após faça uma serie de flexão sem descanso"}, 
-        { id: "crossover", nome: "CrossOver", imagem: "imgs\crossover.png", series: "3x10", descricao: "Vá na polia, pegue os dois lados, leve para o alto, e empurre como se fosse uma barra"}
+        { id: "supino_reto_halteres", nome: "Supino Reto com Halteres", imagem: "imgs/supino_reto_halteres.png", series: "4x10", descricao: "Pegue o banco, deixe reto, e empurre halteres para cima." },
+        { id: "supino_inclinado_halteres", nome: "Supino Inclinado com Halteres", imagem: "imgs/supino_inclinado_halteres.png", series: "4x10", descricao: "Pegue o banco, deixe Inclinado, e empurre halteres para cima." }, 
+        { id: "peckdeck_flexao", nome: "PeckDeck com Flexão", imagem: "imgs/peckdeck_flexao.png", series: "3x10+10", descricao: "Faça o PeckDeck e logo após faça uma série de flexão sem descanso" }, 
+        { id: "crossover", nome: "CrossOver", imagem: "imgs/crossover.png", series: "3x10", descricao: "Vá na polia, pegue os dois lados, leve para o alto, e empurre como se fosse uma barra" }
     ],
     "triceps": [
-        { id: "triceps_corda", nome: "Tríceps Corda", imagem: "imgs\triceps_corda.png", series: "4x12", descricao: "Use a corda para estender os braços." },
-        { id: "triceps_barra_dropset", nome: "Tríceps Barra DropSet", imagem: "imgs\triceps_barra_dropset.png", series: "4x12", descricao: "Use a corda para estender os braços." },
-        { id: "triceps_mergulho", nome: "Tríceps Mergulho", imagem: "imgs\triceps_mergulho.png", series: "4x12", descricao: "Use a corda para estender os braços." }
+        { id: "triceps_corda", nome: "Tríceps Corda", imagem: "imgs/triceps_corda.png", series: "4x12", descricao: "Use a corda para estender os braços." },
+        { id: "triceps_barra_dropset", nome: "Tríceps Barra DropSet", imagem: "imgs/triceps_barra_dropset.png", series: "4x12", descricao: "Coloque a barrinha na polia, no alto, e na ultima, repete o exercicio diminuindo carga." },
+        { id: "triceps_mergulho", nome: "Tríceps Mergulho", imagem: "imgs/triceps_mergulho.png", series: "4x12", descricao: "Vá até o banco, e apoiando nele, desça até quase o chão." }
     ],
     "costas": [
-        { id: "puxada_frontal", nome: "Puxada Frontal", imagem: "puxada-frontal.jpg", series: "4x12", descricao: "Puxe a barra para frente do corpo." }
+        { id: "puxada_frontal", nome: "Puxada Frontal", imagem: "imgs/puxada-frontal.jpg", series: "4x12", descricao: "Puxe a barra para frente do corpo." }
     ],
     "pernas": [
-        { id: "agachamento", nome: "Agachamento", imagem: "agachamento.jpg", series: "4x10", descricao: "Agache com a barra nas costas." }
+        { id: "agachamento", nome: "Agachamento", imagem: "imgs/agachamento.jpg", series: "4x10", descricao: "Agache com a barra nas costas." }
     ]
     // Adicione mais grupos musculares e exercícios aqui
 };
