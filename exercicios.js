9057424594f9bf1b79ae33ced64e411d16ec9141
Pedro Bidoia <pedro.bidoias@gmail.com>
2024-10-06 13:17:36 -0300

    PeitoPedro

--- exercicios.js
+++ exercicios.js
@@ -1,7 +1,9 @@
 const todosExercicios = {
     "peito": [
-        { id: "supino_reto", nome: "Supino Reto", imagem: "supino-reto.jpg", series: "4x10", descricao: "Empurre a barra reta para cima." },
-        { id: "supino_inclinado", nome: "Supino Inclinado", imagem: "supino-inclinado.jpg", series: "4x10", descricao: "Empurre a barra inclinada." }
+        { id: "supino_reto_halteres", nome: "Supino Reto com Halteres", imagem: "imgs\supino_reto_halteres.png", series: "4x10", descricao: "Pegue o banco, deixe reto, e empurre halteres para cima." },
+        { id: "supino_inclinado_halteres", nome: "Supino Inclinado com Halteres", imagem: "imgs\supino_inclinado_halteres.png", series: "4x10", descricao: "Pegue o banco, deixe Inclinado, e empurre halteres para cima."}, 
+        { id: "peckdeck_flexao", nome: "PeckDeck com Flexão", imagem: "imgs\peckdeck_flexao.png", series: "3x10+10", descricao: "Faça o PeckDeck e logo após faça uma serie de flexão sem descanso"}, 
+        { id: "crossover", nome: "CrossOver", imagem: "imgs\crossover.png", series: "3x10", descricao: "Vá na polia, pegue os dois lados, leve para o alto, e empurre como se fosse uma barra"}
     ],
     "triceps": [
         { id: "triceps_corda", nome: "Tríceps Corda", imagem: "triceps-corda.jpg", series: "4x12", descricao: "Use a corda para estender os braços." }
