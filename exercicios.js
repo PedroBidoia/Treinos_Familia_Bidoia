04b6a9abecbab087a9dcbf0e1b0497051452efca
Pedro Bidoia <pedro.bidoias@gmail.com>
2024-10-06 18:15:45 -0300

    AtualizaçãoTreinoAPedro

--- exercicios.js
+++ exercicios.js
@@ -1,13 +1,18 @@
 const todosExercicios = {
     "peito": [
         { id: "supino_reto_halteres", nome: "Supino Reto com Halteres", imagem: "imgs/supino_reto_halteres.png", series: "4x10", descricao: "Pegue o banco, deixe reto, e empurre halteres para cima." },
+        { id: "supino_inclinado", nome: "Supino Inclinado", imagem: "imgs/supino_inclinado.png", series: "4x10", descricao: "Pegue o banco, deixe Inclinado, e empurre a barra para cima." },
         { id: "supino_inclinado_halteres", nome: "Supino Inclinado com Halteres", imagem: "imgs/supino_inclinado_halteres.png", series: "4x10", descricao: "Pegue o banco, deixe Inclinado, e empurre halteres para cima." }, 
-        { id: "peckdeck_flexao", nome: "PeckDeck com Flexão", imagem: "imgs/peckdeck_flexao.png", series: "3x10+10", descricao: "Faça o PeckDeck e logo após faça uma série de flexão sem descanso" }, 
+        { id: "peckdeck_flexao", nome: "PeckDeck com Flexão", imagem: "imgs/peckdeck_flexao.png", series: "3x10+10", descricao: "Faça o PeckDeck e logo após faça uma série de flexão sem descanso" },
+        { id: "pullover", nome: "Pull Over", imagem: "imgs/pullover.png", series: "3x10", descricao: "Deixe o banco reto, pegue o haltere, e faça o movimento sem dobrar o braço" }, 
+        { id: "crucifixo_polia_flexao", nome: "PeckDeck com Flexão", imagem: "imgs/crucifixo_polia_flexao.png", series: "3x10+10", descricao: "Faça o crucifixo com as polias na altura dos ombros e logo após faça uma série de flexão sem descanso" },
         { id: "crossover", nome: "CrossOver", imagem: "imgs/crossover.png", series: "3x10", descricao: "Vá na polia, pegue os dois lados, leve para o alto, e empurre como se fosse uma barra" }
     ],
     "triceps": [
         { id: "triceps_corda", nome: "Tríceps Corda", imagem: "imgs/triceps_corda.png", series: "4x12", descricao: "Use a corda para estender os braços." },
+        { id: "triceps_testa_polia", nome: "Tríceps Barra DropSet", imagem: "imgs/triceps_testa_polia.png", series: "4x12", descricao: "Coloque o triângulo na polia, na altura da cabeça, e faça como se fosse um testa com halteres" },
         { id: "triceps_barra_dropset", nome: "Tríceps Barra DropSet", imagem: "imgs/triceps_barra_dropset.png", series: "4x12", descricao: "Coloque a barrinha na polia, no alto, e na ultima, repete o exercicio diminuindo carga." },
+        { id: "triceps_frances_polia", nome: "Tríceps Frances na Polia", imagem: "imgs/triceps_frances_polia.png", series: "4x12", descricao: "Desça a polia até o chão, e unilateralmente, faça o francês" },
         { id: "triceps_mergulho", nome: "Tríceps Mergulho", imagem: "imgs/triceps_mergulho.png", series: "4x12", descricao: "Vá até o banco, e apoiando nele, desça até quase o chão." }
     ],
     "costas": [
